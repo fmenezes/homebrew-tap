6962b3efe9a9e6c8387d31b88039098a235f0674
Filipe Menezes <fcmenezes87@gmail.com>
2022-05-24 14:19:39 +0100

    Add british_latin_keyboard cask

---
+++ Casks/british-latin-keyboard.rb
@@ -0,0 +1,16 @@
+cask "british-latin-keyboard" do
+  version "v2.0.1"
+  sha256 "ebf6a50d8b2f848634c5d245ca64c411b31df972802d4ca962a76bc653568fcf"
+
+  url "https://github.com/fmenezes/british-macos-custom-keyboard-layout/releases/download/#{version}/British.Latin.pkg"
+  name "british_latin_keyboard"
+  desc "This is a custom keyboard layout to allow typing Latin chars in the British keyboard for Macbook. Also compatible with Apple external keyboard in British English keyboard layout."
+  homepage "https://github.com/fmenezes/british-macos-custom-keyboard-layout"
+
+  pkg "British.Latin.pkg"
+
+  uninstall pkgutil:   [
+    "British.Latin.pkg"
+  ]
+
+end
