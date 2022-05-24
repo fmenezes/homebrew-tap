9dc07761aa95aa225d6c25aeb06f36520bb86a4f
Filipe Menezes <fcmenezes87@gmail.com>
2022-05-24 14:19:39 +0100

    british-latin-keyboard v2.0.1 (new cask)
    * Add british_latin_keyboard cask
    * remove uninstall block
    * fix style
    * add uninstall section
    * fix style
    
    Closes #3.
    
    Signed-off-by: Filipe Constantinov Menezes <1765216+fmenezes@users.noreply.github.com>

---
+++ Casks/british-latin-keyboard.rb
@@ -0,0 +1,15 @@
+cask "british-latin-keyboard" do
+  version "v2.0.1"
+  sha256 "ebf6a50d8b2f848634c5d245ca64c411b31df972802d4ca962a76bc653568fcf"
+
+  url "https://github.com/fmenezes/british-macos-custom-keyboard-layout/releases/download/#{version}/British.Latin.pkg"
+  name "british_latin_keyboard"
+  desc "This is a custom en-gb keyboard layout to allow typing Latin chars"
+  homepage "https://github.com/fmenezes/british-macos-custom-keyboard-layout"
+
+  pkg "British.Latin.pkg"
+
+  uninstall pkgutil: [
+    "org.sil.ukelele.keyboardlayout.british(latin)",
+  ]
+end
